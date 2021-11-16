4065c8245177f0d04dd9ae2ed2f09d9f9e014c8a
José Israel Santizo Santos <joseisraelss.JISS@gmail.com>
2021-11-16 15:30:52 -0600

    Se declaran variables secundarias y se agrega la funcion onload para cargar un pais en concreto

--- js/main.js
+++ js/main.js
@@ -10,3 +10,18 @@ let timeZone = document.getElementById('timezone');
 let date = document.getElementById('date');
 let min = document.getElementById('min');
 let max = document.getElementById('max');
+//Declara funciones secundarias
+const displayBackgroundImage = () => {}
+const displayData = () => {}
+//Declaracion weatherData
+const getWeatherData = (city) => {
+    //Hacer un request a la API y obtener un objeto que tenga los datos solicitados de la cuidad
+    //Cambiar la imagen del fondo de pantalla dependiendo el clima
+    //Mostrar datos en la pantalla al cargar la pagina con las variables de DOM
+    displayBackgroundImage(data);
+    displayData(data);
+}
+//El ingresar a la pagina aparece un pais enconcreto
+windows.onload = () => {
+    getWeatherData("Guatemala");
+}
